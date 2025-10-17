4063bcd228e816e29aecee86726569c683eb4bbc
Himanshu <himanshu21464@iiitd.ac.in>
2025-10-17 19:47:22 +0530

    Header section complete

--- Frontend/Food_Delivery/src/App.jsx
+++ Frontend/Food_Delivery/src/App.jsx
@@ -1,6 +1,10 @@
 import Navbar from "./components/Navbar/Navbar";
 import "./App.css";
-import { Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+
 
 const App = () => {
 
--- Frontend/Food_Delivery/src/pages/Home/Home.jsx
+++ Frontend/Food_Delivery/src/pages/Home/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './Home.css'
+import Header from '../../components/Header/Header'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="home">
+      <Header />
+
+    </div>
   )
 }
+
+
 
 export default Home
