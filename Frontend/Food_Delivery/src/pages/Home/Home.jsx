080e6ab5fbff5eac2a9ff79d9ad719f9c8693d9f
Himanshu <himanshu21464@iiitd.ac.in>
2025-10-18 21:23:47 +0530

    added the food items list

--- Frontend/Food_Delivery/src/pages/Home/Home.jsx
+++ Frontend/Food_Delivery/src/pages/Home/Home.jsx
@@ -2,14 +2,20 @@ import React from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 
 function Home() {
+
+  const [category, setCategory] = React.useState('All');
+
+
+
   return (
     <div className="home">
       <Header />
-      <ExploreMenu />
-
-    </div>
+      <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category}/>
+          </div>
   )
 }
 
